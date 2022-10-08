310ab14b46b48f41e1978e414e069d49294ea745
samuelmalik <samuelmalik9@gmail.com>
2022-10-08 22:43:39 +0200

    added function alternating capitalization

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -6,7 +6,27 @@ namespace Open_Lab_05._05
     {
         public string AlternatingCaps(string original)
         {
-            throw new NotImplementedException();
+            string result = "";
+            int x = 0;
+            for (int i = 0; i < original.Length; i++)
+            {
+                if (original[i] == ' ')
+                {
+                    result += " ";
+                    continue;
+                }
+                if (x%2 == 0)
+                {
+                   result += char.ToUpper(original[i]);
+                    x++;
+                }
+                else
+                {
+                    result += char.ToLower(original[i]);
+                    x++;
+                }
+            }
+            return result;
         }
     }
 }
